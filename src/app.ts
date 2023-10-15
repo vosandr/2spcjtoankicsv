7502f527e8eb9ef223b8accf43c59e7622a9549f
Andrey <vosirandr@gmail.com>
2023-10-16 00:10:49 +0300

    add code

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,16 @@ import readline from 'readline';
 import fs from 'fs';
 export default (inputPath: string, outputPath: string) => {
   const rl = readline.createInterface({ 
-    input: fs.createReadStream(__dirname+'/'+inputPath),
-    output: fs.createWriteStream(__dirname+'/'+outputPath)
+    input: fs.createReadStream(inputPath),
+    output: fs.createWriteStream(outputPath)
   }); 
   rl.on('line', (line) => {
-      line.includes("\t")===true?
-      line.replace('\t', '<br>'):
-      line.replace('/^/', '"')&&line.replace('/$/', ';"')
-    fs.appendFileSync(__dirname+'/'+outputPath, line)
+    if(line.includes("\t")===true){
+      fs.appendFileSync(outputPath, line.replace('\t', '<br>'))
     }
-  )
+    else{
+      fs.appendFileSync(outputPath, line.replace('/^/', '"'))
+      fs.appendFileSync(outputPath, line.replace('/$/', ';"'))
+    }
+  })
 }
