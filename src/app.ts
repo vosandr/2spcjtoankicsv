8591fde15acc7a40f737eeb17fa09a2a94b67957
Vosandr <vosirandr@gmail.com>
2023-10-18 14:17:16 +0300

    add code

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,19 @@ export default (inputPath: string, outputPath: string) => {
   const writeStream=fs.createWriteStream(outputPath)
   rl.on('line', (line) => {
     if(line.includes("\t")===true){
-      writeStream.write(line.replace('  ', '<br>'))
+      writeStream.write(line.replace('  ', '<br>'));
     }
     else{
       writeStream.write(line.replace('/^/', '"'))
       writeStream.write(line.replace('/$/', ';"'+EOL));
     }
+  })
+  const rl1stline = readline.createInterface({
+    input: fs.createReadStream(outputPath),
+    crlfDelay: Infinity,
+  });
+  rl1stline.on('line', (line)=> {
+    fs.writeFileSync('outputFile', line.replace('/^"/', ''));
+    rl1stline.close();
   })
 }
